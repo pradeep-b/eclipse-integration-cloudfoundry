1171be62ba924bbc4e15527203c11b3ae56ab43e
Pradeep <Pradeep@9.109.127.168>
2015-10-13 16:07:00 +0530

    Initial commit with splitting out UI from branding

---
+++ org.eclipse.cft.server.ui/src/org/eclipse/cft/server/ui/internal/CloudFoundryBrandingUIExtensionPoint.java
@@ -0,0 +1,92 @@
+package org.eclipse.cft.server.ui.internal;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.cft.server.core.internal.CloudFoundryBrandingExtensionPoint;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExtension;
+import org.eclipse.core.runtime.IExtensionPoint;
+import org.eclipse.core.runtime.Platform;
+
+public class CloudFoundryBrandingUIExtensionPoint extends CloudFoundryBrandingExtensionPoint {
+
+	public static String ATTR_NAME = "name"; //$NON-NLS-1$
+
+	public static String ATTR_SERVER_TYPE_ID = "serverTypeId"; //$NON-NLS-1$
+
+	public static String ATTR_WIZ_BAN = "wizardBanner"; //$NON-NLS-1$
+
+	public static String POINT_ID = "org.eclipse.cft.server.ui.brandingUI"; //$NON-NLS-1$
+
+	private static Map<String, IConfigurationElement> brandingUIDefinitions = new HashMap<String, IConfigurationElement>();
+	
+	private static List<String> brandingUIServerTypeIds = new ArrayList<String>();
+
+	private static boolean read;
+
+	private static void readBrandingUIDefinitions() {
+		IExtensionPoint brandingUIExtPoint = Platform.getExtensionRegistry().getExtensionPoint(POINT_ID);
+		if (brandingUIExtPoint != null) {
+			
+			// Ensure core branding is initialized first
+			CloudFoundryBrandingExtensionPoint.readBrandingDefinitions();
+			
+			brandingUIServerTypeIds.clear();
+			for (IExtension extension : brandingUIExtPoint.getExtensions()) {
+				for (IConfigurationElement config : extension.getConfigurationElements()) {
+					String serverId = config.getAttribute(ATTR_SERVER_TYPE_ID);
+					String name = config.getAttribute(ATTR_NAME);
+					if (serverId != null && serverId.trim().length() > 0 && name != null && name.trim().length() > 0) {
+						// Do the same checks as in CloudFoundryBrandingExtensionPoint#readBrandingDefinitions()
+						IConfigurationElement coreConfig = CloudFoundryBrandingExtensionPoint.getConfigurationElement(serverId);
+						if (coreConfig != null) {
+							IConfigurationElement[] defaultUrl = coreConfig.getChildren(ELEM_DEFAULT_URL);
+							IConfigurationElement[] cloudUrls = coreConfig.getChildren(ELEM_CLOUD_URL);
+							String urlProviderClass = coreConfig.getAttribute(ATTR_URL_PROVIDER_CLASS);
+							if ((defaultUrl != null && defaultUrl.length > 0)
+									|| (cloudUrls != null && cloudUrls.length > 0) || urlProviderClass != null) {
+								brandingUIDefinitions.put(serverId, config);
+								brandingUIServerTypeIds.add(serverId);
+							}
+						}
+					}
+				}
+			}
+			read = true;
+		}
+	}
+
+	public static List<String> getServerTypeIds() {
+		if (!read) {
+			readBrandingUIDefinitions();
+		}
+
+		return brandingUIServerTypeIds;
+	}
+
+	public static String getServiceName(String serverTypeId) {
+		if (!read) {
+			readBrandingUIDefinitions();
+		}
+		IConfigurationElement config = brandingUIDefinitions.get(serverTypeId);
+		if (config != null) {
+			return config.getAttribute(ATTR_NAME);
+		}
+		return null;
+	}
+
+	public static String getWizardBannerPath(String serverTypeId) {
+		if (!read) {
+			readBrandingUIDefinitions();
+		}
+		IConfigurationElement config = brandingUIDefinitions.get(serverTypeId);
+		if (config != null) {
+			return config.getAttribute(ATTR_WIZ_BAN);
+		}
+		return null;
+	}
+
+}
